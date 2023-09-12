270f0e1abf1d906687dc29f1aae8109c068ad099
Jeral Pineda <jpineda@banhcafe.hn>
2023-09-12 16:28:23 -0600

    style: add global styles

---
+++ src/theme/theme.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet} from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  margin: {
+    marginHorizontal: 20,
+  },
+  pokebolaBg: {
+    position: "absolute",
+    width: 300,
+    height: 300,
+    top: -100,
+    right: -100,
+    opacity: 0.2,
+  },
+  text: {
+    color: "#000",
+  },
+
+  title: {
+    color: "#000",
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+});
