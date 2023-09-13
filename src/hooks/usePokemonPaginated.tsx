7a60a1e93778f13d79ab18f6d0ecd9ec8d4c3d26
Jeral Pineda <jpineda@banhcafe.hn>
2023-09-13 08:36:52 -0600

    feat: agregar los tipos de datos que retorna la peticion http

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,12 +1,30 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
+
 import {pokemonApi} from "../api/pokemon";
+import {
+  Pokemon,
+  PokemonPaginatedResponse,
+  SimplePokemon,
+} from "../types/pokemon";
 
 export const usePokemonPaginated = () => {
-  const nextPageUrl = useRef(`https://pokeapi.co/api/v2/pokemon?limit=40`);
+  const [simplePokemons, setSimplePokemons] = useState<SimplePokemon[]>([]);
+  const nextPageUrl = useRef("https://pokeapi.co/api/v2/pokemon?limit=40");
 
   const loadPokemons = async () => {
-    const resp = await pokemonApi.get(nextPageUrl.current);
-    console.log(JSON.stringify(resp.data, null, 3));
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current,
+    );
+
+    //Almacenar la siguiente pagina
+    nextPageUrl.current = resp.data.next;
+
+    //Modificar la data de los pokemons
+    mapPokemons(resp.data.results);
+  };
+
+  const mapPokemons = (pokemons: Pokemon[]) => {
+    pokemons.forEach(poke => console.log(poke.name));
   };
 
   useEffect(() => {
