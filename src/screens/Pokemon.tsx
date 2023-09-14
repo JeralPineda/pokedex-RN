733a205a74e19b969d725a925ec4248bfdbe3b8e
Jeral Pineda <jpineda@banhcafe.hn>
2023-09-14 15:38:52 -0600

    feat: Trabajando conb la pantalla del pokemon

--- src/screens/Pokemon.tsx
+++ src/screens/Pokemon.tsx
@@ -1,21 +1,92 @@
 import {StackScreenProps} from "@react-navigation/stack";
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, TouchableOpacity, Image} from "react-native";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import LinearGradient from "react-native-linear-gradient";
+import Icon from "react-native-vector-icons/Ionicons";
+
 import {RootStackParams} from "../navigation/AppNavigation";
-import {globalStyles} from "../theme";
+import {FadeInImage} from "../components";
 
 interface PokemonProps extends StackScreenProps<RootStackParams, "Pokemon"> {}
 
 export const Pokemon = ({navigation, route}: PokemonProps) => {
   const {colors, simplePokemon} = route.params;
+  const {name, id, picture} = simplePokemon;
 
-  console.log("colors", JSON.stringify(simplePokemon, null, 3));
-  console.log(colors);
+  const {top} = useSafeAreaInsets();
+
   return (
     <View>
-      <Text style={globalStyles.text}>Pokemon: {simplePokemon.name}</Text>
+      {/* Header Container */}
+      <View style={{...styles.headerContainer}}>
+        {/* Gradient */}
+        <LinearGradient
+          colors={[colors.secondary, colors.primary]}
+          start={{x: 0.1, y: 0.1}}
+          end={{x: 0.5, y: 0.5}}
+          style={{...StyleSheet.absoluteFillObject, ...styles.gradient}}
+        />
+
+        {/* Back Buttom */}
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={{...styles.backButtom, top: top + 5}}
+          onPress={() => navigation.pop()}>
+          <Icon name="arrow-back-outline" color="white" size={26} />
+        </TouchableOpacity>
+
+        {/* Pokemon Name */}
+        <Text style={{...styles.pokemonName, top: top + 40}}>
+          {name + "\n#"}
+          {id}
+        </Text>
+
+        {/* Pokebola Blanca */}
+        <Image
+          source={require("../assets/pokebola-blanca.png")}
+          style={styles.pokeball}
+        />
+
+        <FadeInImage uri={picture} style={styles.pokemonImage} />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 370,
+    zIndex: 999,
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000,
+    alignItems: "center",
+  },
+  gradient: {
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000,
+  },
+  backButtom: {
+    position: "absolute",
+    left: 20,
+  },
+  pokemonName: {
+    color: "#fff",
+    fontSize: 40,
+    alignSelf: "flex-start",
+    left: 20,
+    textTransform: "capitalize",
+  },
+  pokeball: {
+    height: 250,
+    width: 250,
+    bottom: -20,
+    opacity: 0.7,
+  },
+  pokemonImage: {
+    width: 250,
+    height: 250,
+    position: "absolute",
+    bottom: -15,
+  },
+});
